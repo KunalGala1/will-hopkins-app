2487ad602ebf582ec711e1ec80f8aac679caf4be
Kunal Gala <kunal.gala16@gmail.com>
2023-06-27 18:42:44 -0400

    ensure auth

--- routes/cms.js
+++ routes/cms.js
@@ -17,8 +17,8 @@ router.get(
     const news = await Content.findOne({ name: 'news' });
 
     res.render('cms/dashboard', {
-      // name: req.user.username,
-      name: 'admin',
+      name: req.user.username,
+      // name: 'admin',
       quote,
       intro,
       news,
@@ -26,181 +26,133 @@ router.get(
   }
 );
 
-router.put(
-  '/quote/:id',
-  // ensureAuthenticated,
-  async (req, res) => {
-    try {
-      const data = await Content.findByIdAndUpdate(
-        req.params.id,
-        { json: JSON.stringify(req.body) },
-        { new: true }
-      );
-      res.json({ success: true, data });
-    } catch (error) {
-      res.status(500).json({ success: false, error: 'Something went wrong' });
-    }
+router.put('/quote/:id', ensureAuthenticated, async (req, res) => {
+  try {
+    const data = await Content.findByIdAndUpdate(
+      req.params.id,
+      { json: JSON.stringify(req.body) },
+      { new: true }
+    );
+    res.json({ success: true, data });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Something went wrong' });
   }
-);
+});
 
-router.put(
-  '/intro/:id',
-  // ensureAuthenticated,
-  async (req, res) => {
-    try {
-      const data = await Content.findByIdAndUpdate(
-        req.params.id,
-        { json: JSON.stringify(req.body) },
-        { new: true }
-      );
-      res.json({ success: true, data });
-    } catch (error) {
-      res.status(500).json({ success: false, error: 'Something went wrong' });
-    }
+router.put('/intro/:id', ensureAuthenticated, async (req, res) => {
+  try {
+    const data = await Content.findByIdAndUpdate(
+      req.params.id,
+      { json: JSON.stringify(req.body) },
+      { new: true }
+    );
+    res.json({ success: true, data });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Something went wrong' });
   }
-);
+});
 
-router.put(
-  '/news/:id',
-  // ensureAuthenticated,
-  async (req, res) => {
-    try {
-      const data = await Content.findByIdAndUpdate(
-        req.params.id,
-        { json: JSON.stringify(req.body) },
-        { new: true }
-      );
-      res.json({ success: true, data });
-    } catch (error) {
-      res.status(500).json({ success: false, error: 'Something went wrong' });
-    }
+router.put('/news/:id', ensureAuthenticated, async (req, res) => {
+  try {
+    const data = await Content.findByIdAndUpdate(
+      req.params.id,
+      { json: JSON.stringify(req.body) },
+      { new: true }
+    );
+    res.json({ success: true, data });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Something went wrong' });
   }
-);
+});
 
 /* Events */
-router.get(
-  '/events',
-  // ensureAuthenticated,
-  async (req, res) => {
-    const events = await Event.find();
-    res.render('cms/events', {
-      events,
+router.get('/events', ensureAuthenticated, async (req, res) => {
+  const events = await Event.find();
+  res.render('cms/events', {
+    events,
+  });
+});
+
+router.post('/events', ensureAuthenticated, async (req, res) => {
+  try {
+    const newEvent = await Event.create({
+      json: JSON.stringify(req.body),
     });
-  }
-);
-
-router.post(
-  '/events',
-  // ensureAuthenticated,
-  async (req, res) => {
-    try {
-      const newEvent = await Event.create({
-        json: JSON.stringify(req.body),
-      });
-      res.json({
-        success: true,
-        newEvent,
-        reaction: 'newEvent',
-      });
-    } catch (error) {
-      res.status(500).json({ success: false, error: 'Something went wrong' });
-    }
-  }
-);
-
-router.get(
-  '/events/new',
-  // ensureAuthenticated,
-  async (req, res) => {
-    res.render('cms/events/new_event');
-  }
-);
-
-router.get(
-  '/events/:id/edit',
-  // ensureAuthenticated,
-  async (req, res) => {
-    const event = await Event.findById(req.params.id);
-    res.render('cms/events/edit_event', {
-      event,
+    res.json({
+      success: true,
+      newEvent,
+      reaction: 'newEvent',
     });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Something went wrong' });
   }
-);
+});
 
-router.get(
-  '/events/:id',
-  // ensureAuthenticated,
-  async (req, res) => {
-    const event = await Event.findById(req.params.id);
-    res.json({ success: true, event });
-  }
-);
+router.get('/events/new', ensureAuthenticated, async (req, res) => {
+  res.render('cms/events/new_event');
+});
 
-router.put(
-  '/events/:id',
-  // ensureAuthenticated,
-  async (req, res) => {
-    try {
-      const updatedEvent = await Event.findByIdAndUpdate(
-        req.params.id,
-        { json: JSON.stringify(req.body) },
-        { new: true }
-      );
-      res.json({ success: true, updatedEvent, reaction: 'updatedEvent' });
-    } catch (error) {
-      res.status(500).json({ success: false, error: 'Something went wrong' });
-    }
-  }
-);
+router.get('/events/:id/edit', ensureAuthenticated, async (req, res) => {
+  const event = await Event.findById(req.params.id);
+  res.render('cms/events/edit_event', {
+    event,
+  });
+});
 
-router.delete(
-  '/events/:id/delete',
-  // ensureAuthenticated,
-  async (req, res) => {
-    try {
-      const deletedEvent = await Event.findByIdAndDelete(req.params.id);
-      res.json({
-        success: true,
-        deletedEvent,
-        reaction: 'deletedEvent',
-      });
-    } catch (error) {
-      res.status(500).json({ success: false, error: 'Something went wrong' });
-    }
+router.get('/events/:id', ensureAuthenticated, async (req, res) => {
+  const event = await Event.findById(req.params.id);
+  res.json({ success: true, event });
+});
+
+router.put('/events/:id', ensureAuthenticated, async (req, res) => {
+  try {
+    const updatedEvent = await Event.findByIdAndUpdate(
+      req.params.id,
+      { json: JSON.stringify(req.body) },
+      { new: true }
+    );
+    res.json({ success: true, updatedEvent, reaction: 'updatedEvent' });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Something went wrong' });
   }
-);
+});
+
+router.delete('/events/:id/delete', ensureAuthenticated, async (req, res) => {
+  try {
+    const deletedEvent = await Event.findByIdAndDelete(req.params.id);
+    res.json({
+      success: true,
+      deletedEvent,
+      reaction: 'deletedEvent',
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Something went wrong' });
+  }
+});
 
 // bio
 
-router.get(
-  '/bio',
-  // ensureAuthenticated,
-  async (req, res) => {
-    const data = await Content.findOne({ name: 'bio' });
-    const json = JSON.parse(data.json);
-    res.render('cms/bio', {
-      quote: json.quote,
-      bio: json.bio,
-      id: data.id,
-    });
-  }
-);
+router.get('/bio', ensureAuthenticated, async (req, res) => {
+  const data = await Content.findOne({ name: 'bio' });
+  const json = JSON.parse(data.json);
+  res.render('cms/bio', {
+    quote: json.quote,
+    bio: json.bio,
+    id: data.id,
+  });
+});
 
-router.put(
-  '/bio/:id',
-  // ensureAuthenticated,
-  async (req, res) => {
-    try {
-      const updatedBio = await Content.findByIdAndUpdate(
-        req.params.id,
-        { json: JSON.stringify(req.body) },
-        { new: true }
-      );
-      res.json({ success: true, data: updatedBio });
-    } catch (error) {
-      res.status(500).json({ success: false, error: 'Something went wrong' });
-    }
+router.put('/bio/:id', ensureAuthenticated, async (req, res) => {
+  try {
+    const updatedBio = await Content.findByIdAndUpdate(
+      req.params.id,
+      { json: JSON.stringify(req.body) },
+      { new: true }
+    );
+    res.json({ success: true, data: updatedBio });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Something went wrong' });
   }
-);
+});
 
 module.exports = router;
