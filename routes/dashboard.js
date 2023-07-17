a2328246e959a2d3979bea8589cd44de772d917a
Aya Yuasa <aya.yuasa98@gmail.com>
2023-07-17 00:00:15 -0400

    refactored routes successfully

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,25 +1,25 @@
 /* Import Required Modules */
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
 /* Import Models */
-const Content = require('../models/Content');
-const Event = require('../models/Event');
-const Work = require('../models/Work');
-const Video = require('../models/Video');
+const Content = require("../models/Content");
+const Event = require("../models/Event");
+const Work = require("../models/Work");
+const Video = require("../models/Video");
 
 /* Import Auth Configs */
-const { forwardAuthenticated } = require('../config/auth');
-const { ensureAuthenticated } = require('../config/auth');
+const { forwardAuthenticated } = require("../config/auth");
+const { ensureAuthenticated } = require("../config/auth");
 
 /* Dashboard */
-router.get('/', ensureAuthenticated, async (req, res) => {
-  const quote = await Content.findOne({ name: 'quote' });
-  const intro = await Content.findOne({ name: 'intro' });
-  const news = await Content.findOne({ name: 'news' });
+router.get("/", ensureAuthenticated, async (req, res) => {
+  const quote = await Content.findOne({ name: "quote" });
+  const intro = await Content.findOne({ name: "intro" });
+  const news = await Content.findOne({ name: "news" });
 
-  res.render('admin/dashboard', {
+  res.render("admin/dashboard", {
     name: req.user.username,
     quote,
     intro,
@@ -28,40 +28,52 @@ router.get('/', ensureAuthenticated, async (req, res) => {
 });
 
 /* Edit Quote */
-router.put('/quote/:id', ensureAuthenticated, async (req, res) => {
+router.put("/quote/:id", ensureAuthenticated, async (req, res) => {
   try {
-    const data = await Content.findByIdAndUpdate(req.params.id, { body: JSON.stringify(req.body) }, { new: true });
+    const data = await Content.findByIdAndUpdate(
+      req.params.id,
+      { body: JSON.stringify(req.body) },
+      { new: true }
+    );
     res.json({ success: true, data });
   } catch (error) {
-    res.status(500).json({ success: false, error: 'Something went wrong' });
+    res.status(500).json({ success: false, error: "Something went wrong" });
   }
 });
 
 /* Edit Intro */
-router.put('/intro/:id', ensureAuthenticated, async (req, res) => {
+router.put("/intro/:id", ensureAuthenticated, async (req, res) => {
   try {
-    const data = await Content.findByIdAndUpdate(req.params.id, { body: JSON.stringify(req.body) }, { new: true });
+    const data = await Content.findByIdAndUpdate(
+      req.params.id,
+      { body: JSON.stringify(req.body) },
+      { new: true }
+    );
     res.json({ success: true, data });
   } catch (error) {
-    res.status(500).json({ success: false, error: 'Something went wrong' });
+    res.status(500).json({ success: false, error: "Something went wrong" });
   }
 });
 
 /* Edit News */
-router.put('/news/:id', ensureAuthenticated, async (req, res) => {
+router.put("/news/:id", ensureAuthenticated, async (req, res) => {
   try {
-    const data = await Content.findByIdAndUpdate(req.params.id, { body: JSON.stringify(req.body) }, { new: true });
+    const data = await Content.findByIdAndUpdate(
+      req.params.id,
+      { body: JSON.stringify(req.body) },
+      { new: true }
+    );
     res.json({ success: true, data });
   } catch (error) {
-    res.status(500).json({ success: false, error: 'Something went wrong' });
+    res.status(500).json({ success: false, error: "Something went wrong" });
   }
 });
 
 /* Bio */
-router.get('/bio', ensureAuthenticated, async (req, res) => {
-  const data = await Content.findOne({ name: 'bio' });
+router.get("/bio", ensureAuthenticated, async (req, res) => {
+  const data = await Content.findOne({ name: "bio" });
   const body = JSON.parse(data.body);
-  res.render('admin/bio', {
+  res.render("admin/bio", {
     quote: body.quote,
     bio: body.bio,
     id: data.id,
@@ -69,12 +81,16 @@ router.get('/bio', ensureAuthenticated, async (req, res) => {
 });
 
 /* Edit Bio */
-router.put('/bio/:id', ensureAuthenticated, async (req, res) => {
+router.put("/bio/:id", ensureAuthenticated, async (req, res) => {
   try {
-    const updatedBio = await Content.findByIdAndUpdate(req.params.id, { body: JSON.stringify(req.body) }, { new: true });
+    const updatedBio = await Content.findByIdAndUpdate(
+      req.params.id,
+      { body: JSON.stringify(req.body) },
+      { new: true }
+    );
     res.json({ success: true, data: updatedBio });
   } catch (error) {
-    res.status(500).json({ success: false, error: 'Something went wrong' });
+    res.status(500).json({ success: false, error: "Something went wrong" });
   }
 });
 
@@ -82,37 +98,116 @@ router.put('/bio/:id', ensureAuthenticated, async (req, res) => {
 
 const models = [
   {
-    name: 'events',
+    name: "events",
   },
   {
-    name: 'listen',
-    model: 'videos',
+    name: "listen",
+    model: "videos",
   },
   {
-    name: 'works',
+    name: "works",
   },
 ];
 
-const Singular_and_Uppercase = string => {
-  return string.slice(0, -1).charAt(0).toUpperCase() + string.slice(0, -1).slice(1);
+const Singular_and_Uppercase = (string) => {
+  return (
+    string.slice(0, -1).charAt(0).toUpperCase() + string.slice(0, -1).slice(1)
+  );
 };
 
-models.forEach(model => {
+models.forEach((model) => {
   /* Define Variables */
   const name = model.name;
   const collection = model.model ? model.model : name;
   const modelName = Singular_and_Uppercase(collection);
+  const singularDocument = modelName.toLowerCase();
 
-  /* Get page */
-  router.get('/' + model.name, ensureAuthenticated, async (req, res) => {
+  /* Get collection page */
+  router.get("/" + name, ensureAuthenticated, async (req, res) => {
     const data = await eval(modelName).find({});
-    res.render('admin/' + model.name, { [collection]: data });
+    res.render("admin/" + name, { [collection]: data });
   });
 
-  /* Add new page */
-  router.get('/' + model.name + '/new', ensureAuthenticated, (req, res) => {
-    res.render('admin/' + model.name + '/new_' + modelName.toLowerCase());
+  /* Add new document page */
+  router.get("/" + name + "/new", ensureAuthenticated, (req, res) => {
+    res.render("admin/" + name + "/new_" + singularDocument);
   });
+
+  /* Add new document */
+  router.post("/" + name, ensureAuthenticated, async (req, res) => {
+    try {
+      const newDocument = await eval(modelName).create({
+        body: JSON.stringify(req.body),
+      });
+      res.json({
+        success: true,
+        newDocument,
+      });
+    } catch (error) {
+      res.status(500).json({ success: false, error: "Something went wrong" });
+    }
+  });
+
+  /* Edit document page */
+  router.get(
+    "/" + name + "/:id/edit",
+    ensureAuthenticated,
+    async (req, res) => {
+      const document = await eval(modelName).findById(req.params.id);
+      res.render("admin/" + name + "/edit_" + singularDocument, {
+        [singularDocument]: document,
+      });
+    }
+  );
+
+  /* Get document */
+  router.get("/" + name + "/:id", ensureAuthenticated, async (req, res) => {
+    const document = await eval(modelName).findById(req.params.id);
+    res.json({ success: true, document });
+  });
+
+  /* Edit document */
+  router.put("/" + name + "/:id", ensureAuthenticated, async (req, res) => {
+    try {
+      const updatedDocument = await eval(modelName).findByIdAndUpdate(
+        req.params.id,
+        {
+          body: JSON.stringify(req.body),
+        },
+        {
+          new: true,
+        }
+      );
+      res.json({ success: true, updatedDocument });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: "Something went wrong",
+      });
+    }
+  });
+
+  /* Delete document */
+  router.delete(
+    "/" + name + "/:id/delete",
+    ensureAuthenticated,
+    async (req, res) => {
+      try {
+        const deletedDocument = await eval(modelName).findByIdAndDelete(
+          req.params.id
+        );
+        res.json({
+          success: true,
+          deletedDocument,
+        });
+      } catch (error) {
+        res.status(500).json({
+          success: false,
+          error: "Something went wrong",
+        });
+      }
+    }
+  );
 });
 
 module.exports = router;
