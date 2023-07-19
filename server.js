053ef981aaaefc821b1b3d30369b84ac3c609e7a
Aya Yuasa <aya.yuasa98@gmail.com>
2023-07-19 18:15:36 -0400

    s3 working

--- server.js
+++ server.js
@@ -1,43 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const multer = require('multer');
-const flash = require('connect-flash');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const flash = require("connect-flash");
+const AWS = require("aws-sdk");
+const { S3Client } = require("@aws-sdk/client-s3");
 
 // db
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
 
-// Multer
-const storage = multer.diskStorage({
-  destination: 'public/uploads',
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-const upload = multer({ storage: storage });
-app.post('/upload', upload.single('file'), (req, res) => {
-  console.log(req.file);
-  res.json({ success: true, file: req.file });
-});
-
 // Passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
-    origin: ['http://localhost:5000'],
+    origin: ["http://localhost:5000"],
     credentials: true,
   })
 );
@@ -45,7 +35,7 @@ app.use(
 // express session
 app.use(
   session({
-    secret: 'secret',
+    secret: "secret",
     resave: true,
     saveUninitialized: true,
   })
@@ -56,15 +46,63 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// AWS
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "us-east-2",
+});
+
+const s3_old = new AWS.S3();
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+  region: "us-east-2",
+});
+
+// Multer
+
+const storage_old = multer.diskStorage({
+  destination: "public/uploads",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload_old = multer({ storage: storage_old });
+app.post("/upload_old", upload_old.single("file"), (req, res) => {
+  console.log(req.file);
+  res.json({ success: true, file: req.file });
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "will-hopkins", // Replace with your S3 bucket name
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (req, file, cb) {
+      cb(null, "uploads/" + Date.now().toString() + "-" + file.originalname);
+    },
+  }),
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log(req.file);
+  res.json({ success: true, file: req.file });
+});
 
 // routes
-app.use('/', require('./routes/index'));
-app.use('/', require('./routes/users'));
-app.use('/', require('./routes/mail'));
-app.use('/dashboard', require('./routes/dashboard'));
+app.use("/", require("./routes/index"));
+app.use("/", require("./routes/users"));
+app.use("/", require("./routes/mail"));
+app.use("/dashboard", require("./routes/dashboard"));
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is listening on port ${PORT}...`);
 });
