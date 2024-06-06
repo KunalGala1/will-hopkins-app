20e5397e50282609ea72ae57b621393b8624b2f6
Kunal Gala <kunal.gala16@gmail.com>
2024-06-06 16:23:33 -0400

    fix: rewrote client-side mail.js to implement cloudflare turnstile

--- public/js/mail.js
+++ public/js/mail.js
@@ -1,58 +1,63 @@
 import { toastNotification } from "./utils.js";
 
+// Temporary - to stop form from sending
 document.addEventListener("DOMContentLoaded", () => {
-  const forms = document.querySelectorAll(".contact-form");
+  const contactForm = document.getElementById("contact-form");
+  if (contactForm) {
+    contactForm.addEventListener("submit", async (event) => {
+      event.preventDefault();
 
-  forms.forEach((form) => {
-    form.addEventListener("submit", async (e) => {
-      // Made the event handler async
-      e.preventDefault();
+      const contactFormData = new FormData(event.target);
 
-      const formData = new FormData(form);
-      let formObject = {};
-
-      for (let [key, value] of formData.entries()) {
-        formObject[key] = value;
+      // Form Validation
+      let contactFormObject = {}; // convert to object
+      for (let [key, value] of contactFormData.entries()) {
+        contactFormObject[key] = value;
       }
 
-      const { name, email, subject } = formObject;
-
-      // Regular expression for email validation
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+      // Check if any field is empty
+      const { name, email, subject } = contactFormObject;
       if (!name || !email || !subject) {
+        toastNotification("Please fill in all fields", "danger", 5000);
+        return;
+      }
+
+      // Check validity of email
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Regular expression for email validation
+      if (!emailRegex.test(email)) {
+        toastNotification("Please enter a valid email address", "danger", 5000);
+        return;
+      }
+
+      // Check if Cloudflare turnstile was completed
+      const cfTurnstileResponse = document.getElementsByClassName(
+        "cf-turnstile-response"
+      );
+      if (!cfTurnstileResponse) {
         toastNotification(
-          "All required fields must be filled out",
+          "Please complete the Cloudflare verification",
           "danger",
           5000
         );
-        return false;
-      } else if (!emailRegex.test(email)) {
-        toastNotification("Please enter a valid email address", "danger", 5000);
-        return false;
+        return;
       }
 
-      // If everything is filled out, send the POST request
-      try {
-        const response = await fetch("/mailsend", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formObject),
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        toastNotification(data.msg, "success", 5000);
-        form.reset();
-      } catch (error) {
-        console.error("There was a problem with the fetch operation:", error);
-        toastNotification(`Error: ${error.message}`, "danger", 5000);
+      // Hit siteverify endpoint to consume turnstile token and check validity
+      const response = await fetch("/verify-turnstile-token", {
+        method: "POST",
+        body: contactFormData,
+      });
+      const result = await response.json();
+      if (!response.ok) {
+        toastNotification(
+          "There was an error sending your email. Please try again later.",
+          "error",
+          5000
+        );
+      } else {
+        toastNotification("Your email was sucessfully sent!", "success", 5000);
+        contactForm.reset();
       }
     });
-  });
+  }
 });
